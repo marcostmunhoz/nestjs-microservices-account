0ad01372f836a708cdf7390e7a3a9ccf9cb7a22b
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2023-05-05 00:58:50 -0300

    fix: import ConfigModule before using it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AccountController } from './account.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AccountController],
   providers: [],
 })
